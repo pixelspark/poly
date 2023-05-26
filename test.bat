fcba5e0fbb084343738b718986920157c8d2201f
Tommy van der Vorst <vandervorst@dialogic.nl>
2023-05-26 17:39:54 +0200

    fix: test script

--- test.bat
+++
@@ -1,1 +0,0 @@
-curl --request POST --url http://localhost:3000/pythia/completion --header 'Content-Type: application/json' --data '{"prompt": "Llamas are ", "max_tokens": 128}'
---
+++ test.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest -Uri "http://localhost:3000/model/pythia/completion" -Method Post -ContentType "application/json" -Body '{"prompt": "Llamas are ", "max_tokens": 128}'
