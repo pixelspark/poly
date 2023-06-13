7ff727e49e9b140fc62ac1aa867c79e05b970a04
Tommy van der Vorst <tommy@pixelspark.nl>
2023-06-13 22:59:53 +0200

    fix: wrong stats for prediction

--- llmd/src/stats.rs
+++ llmd/src/stats.rs
@@ -18,12 +18,16 @@ impl InferenceStatsAdd for InferenceStats {
 
 #[derive(Serialize, Debug, Clone)]
 pub struct TaskStats {
+	/// Number of completion cycles (`Backend::completion`) that were completed for this model
 	cycles: usize,
+
 	/// Total duration of prediction measured in thread-time
+	predict_duration: Duration,
 	predict_duration_threads: Duration,
 	predict_tokens: usize,
 
 	/// Total duration of prompt feeding measured in thread-time
+	prompt_duration: Duration,
 	prompt_duration_threads: Duration,
 	prompt_tokens: usize,
 }
@@ -33,9 +37,11 @@ impl Default for TaskStats {
 		Self {
 			cycles: 0,
 
+			predict_duration: Duration::ZERO,
 			predict_duration_threads: Duration::ZERO,
 			predict_tokens: 0,
 
+			prompt_duration: Duration::ZERO,
 			prompt_duration_threads: Duration::ZERO,
 			prompt_tokens: 0,
 		}
@@ -46,8 +52,11 @@ impl TaskStats {
 	pub fn add_cycle(&mut self, stats: &InferenceStats, n_threads: usize) {
 		self.predict_tokens += stats.predict_tokens;
 		self.prompt_tokens += stats.prompt_tokens;
+		self.prompt_duration += stats.feed_prompt_duration;
 		self.prompt_duration_threads += stats.feed_prompt_duration * (n_threads as u32);
-		self.prompt_duration_threads += stats.predict_duration * (n_threads as u32);
+
+		self.predict_duration += stats.predict_duration;
+		self.predict_duration_threads += stats.predict_duration * (n_threads as u32);
 		self.cycles += 1;
 	}
 }
