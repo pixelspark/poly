8c1de3d7ad7a1654d0417baf0d410f09b0934a0b
Tommy van der Vorst <tommy@pixelspark.nl>
2023-06-14 23:21:00 +0200

    fix: sequence logic

--- llmd/src/sequence.rs
+++ llmd/src/sequence.rs
@@ -17,11 +17,26 @@ impl Sequence {
 		if self.state >= self.tokens.len() {
 			true // Already complete
 		} else {
-			if token.as_bytes().starts_with(&self.tokens.as_bytes()[self.state..]) {
-				self.state += token.len();
+			let remainder = &self.tokens.as_bytes()[self.state..];
+			let overlap_length = remainder.len().min(token.len());
+			if (remainder.len() == token.len() && remainder == token.as_bytes()) || remainder.starts_with(&token.as_bytes()[0..overlap_length]) {
+				self.state += overlap_length;
+				// The unused part of the token (if it was longer than our remainder) can be used to advance once more
+				if token.len() > remainder.len() && self.is_complete() {
+					self.state = 0;
+					self.advance(&token[remainder.len()..]);
+					return true;
+				}
 			} else {
 				// Reset back to zero
-				self.state = 0;
+				if self.state != 0 {
+					// Try again from the beginning if we weren't at zero already
+					self.state = 0;
+					return self.advance(token);
+				} else {
+					// Just reset back to zero
+					self.state = 0;
+				}
 			}
 			self.is_complete()
 		}
@@ -73,6 +88,23 @@ mod test {
 		s.reset();
 		assert!(!s.advance("d"));
 		assert!(!s.advance("e"));
+
 		assert!(s.advance("f"));
+
+		s.reset();
+		assert!(s.advance("defq"));
+
+		s.reset();
+		assert!(s.advance("defde"));
+		assert!(s.advance("f"));
+
+		s.reset();
+		assert!(s.advance("defde"));
+		assert!(s.advance("def"));
+
+		s.reset();
+		assert!(s.advance("defde"));
+		println!("{s:?}");
+		assert!(!s.advance("ef"));
 	}
 }
